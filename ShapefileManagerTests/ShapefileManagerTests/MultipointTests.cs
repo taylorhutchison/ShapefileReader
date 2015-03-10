9131217353b316d3fb6988897a2549ef555772e9
Taylor Hutchison <seanthutchison@gmail.com>
2015-03-10 12:54:49 -0500

    Added Unit Tests Project

---
+++ ShapefileManagerTests/ShapefileManagerTests/MultipointTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ShapefileManagerTests
+{
+    [TestClass]
+    public class MultipointTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ ShapefileManagerTests/ShapefileManagerTests/ShapefileReaderTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GISData.GeometryTypes;
+
+namespace ShapefileManagerTests
+{
+    [TestClass]
+    public class ShapefileReaderTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Point p = new Point(1, 2);
+            Assert.AreEqual(5, p.X);
+        }
+    }
+}
